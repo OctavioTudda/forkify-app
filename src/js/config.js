a4925659a99a8bd94ee545a31605aca0a7391fe4
OctavioTudda <octavio.tudda@hotmail.com>
2022-08-22 12:02:11 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = '74d29b03-cfa9-4fb6-95eb-964ef984d931';
+export const MODAL_CLOSE_SEC = 2.5;
